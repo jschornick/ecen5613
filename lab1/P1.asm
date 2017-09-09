571a1f69b9417177ae7297abcde107176fdafc39
Jeff Schornick <jeff@schornick.org>
2017-09-09 05:09:57 -0600

    [L1] Initial makefile and dosbox configuration

---
+++ lab1/P1.asm
@@ -0,0 +1,12 @@
+; File: P1.asm
+; Author: Jeff Schornick
+;
+; ECEN5613 Lab #1, P1
+;
+; Calculates the result of the following equation using integer division:
+;   Z = ((X+1)/Y)*4
+;
+; The parameters X and Y should be assigned as follows:
+;   X (addend) - > A (accumluator)
+;   Y (divisor) -> B
+
