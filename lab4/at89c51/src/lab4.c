4e97ebca652878738b7f469840971dfd5920ac99
Jeff Schornick <jeff@schornick.org>
2017-11-10 07:16:35 -0700

    [L4] Welcome smiley, fix custom character bit order

--- lab4/at89c51/src/lab4.c
+++ lab4/at89c51/src/lab4.c
@@ -3,6 +3,7 @@
 //
 // Implements the required functionality for ECEN5613 Lab 4.
 //
+
 // Specifically, ...
 //
 // Compilation: Supports SDCC v3.6+, see included makefile for invocation
@@ -18,6 +19,35 @@
 #define printf printf_tiny
 
 #define NIBBLE_TO_ASCII(x) ( (x)<10 ? (x)+'0' : (x)+'A'-10 )
+
+// Command key definitions for the menu
+#define KEY_DDRAM    'D'  // Dump LCD DDRAM
+#define KEY_CGRAM    'G'  // Dump LCD CGRAM
+#define KEY_SCREEN   'S'  // Dump LCD Screen contents
+#define KEY_TYPE     'T'  // Type on LCD
+#define KEY_CLEAR    'C'  // Clear the LCD
+#define KEY_CUSTOM   'N'  // New custom character
+#define KEY_EE_WRITE 'W'  // Write byte to EEPROM
+#define KEY_EE_READ  'R'  // Read byte from EEPROM
+#define KEY_EE_DUMP  'E'  // Dump EEPROM
+#define KEY_SAVE     'V'  // Save LCD data to EEPROM
+#define KEY_LOAD     'L'  // Load LCD data from EEPROM
+
+// Useful ASCII values
+#define ASCII_ESC 0x1b  /* escape */
+#define ASCII_ETX 0x03  /* control-C */
+#define ASCII_CR  0x0d  /* carriage return */
+
+// Control sequences for arrow keys
+#define CS_UP    'A'
+#define CS_DOWN  'B'
+#define CS_LEFT  'D'
+#define CS_RIGHT 'C'
+
+// Typing mode input state machine
+#define TYPE_MODE_NORM 0  /* normal typing */
+#define TYPE_MODE_ESC  1  /* escape sequence */
+#define TYPE_MODE_CS   2  /* control sequence */
 
 void dump_screen(void)
 {
@@ -104,19 +134,6 @@ void dump_cgram(void)
 }
 
 
-#define ASCII_ESC 0x1b
-#define ASCII_ETX 0x03
-#define ASCII_CR  0x0d
-
-#define CS_UP    'A'
-#define CS_DOWN  'B'
-#define CS_LEFT  'D'
-#define CS_RIGHT 'C'
-
-#define TYPE_MODE_NORM 0
-#define TYPE_MODE_ESC  1
-#define TYPE_MODE_CS   2
-
 void cmd_type(void)
 {
   __data uint8_t mode = TYPE_MODE_NORM;
@@ -191,58 +208,35 @@ void cmd_type(void)
 
 }
 
-
-// Command key definitions for the menu
-#define KEY_DDRAM    'D'  // Dump LCD DDRAM
-#define KEY_CGRAM    'G'  // Dump LCD CGRAM
-#define KEY_SCREEN   'S'  // Dump LCD Screen contents
-#define KEY_TYPE     'T'  // Type on LCD
-#define KEY_CLEAR    'C'  // Clear the LCD
-#define KEY_CUSTOM   'N'  // New custom character
-#define KEY_EE_WRITE 'W'  // Write byte to EEPROM
-#define KEY_EE_READ  'R'  // Read byte from EEPROM
-#define KEY_EE_DUMP  'E'  // Dump EEPROM
-#define KEY_SAVE     'V'  // Save LCD data to EEPROM
-#define KEY_LOAD     'L'  // Load LCD data from EEPROM
-
-
 void lcd_welcome(void)
 {
-  // Smiley face
-  lcd_cgram_addr(0x0);
-  lcd_putchar(0x1b);
-  lcd_putchar(0x1b);
-  lcd_putchar(0x00);
-  lcd_putchar(0x04);
-  lcd_putchar(0x04);
-  lcd_putchar(0x11);
-  lcd_putchar(0x0e);
-  lcd_putchar(0x00);
-  lcd_putchar(0x1b);
+  uint8_t smiley[][] = {{0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x03, 0x02},
+                        {0x03, 0x0C, 0x10, 0x00, 0x00, 0x1F, 0x1D, 0x0E},
+                        {0x18, 0x06, 0x01, 0x00, 0x00, 0x1F, 0x1D, 0x0E},
+                        {0x00, 0x00, 0x00, 0x00, 0x10, 0x10, 0x18, 0x08},
+                        {0x02, 0x02, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00},
+                        {0x00, 0x00, 0x08, 0x07, 0x00, 0x10, 0x0C, 0x03},
+                        {0x00, 0x06, 0x0C, 0x18, 0x00, 0x01, 0x06, 0x18},
+                        {0x08, 0x08, 0x10, 0x10, 0x00, 0x00, 0x00, 0x00} };
+  uint8_t i;
+  for(i = 0; i<8; i++) {
+  lcd_create_char(i, smiley[i]);
+  }
 
-  // Corner decorations
   lcd_gotoxy(0,0);
+  lcd_putstr("Happy Friday!");
+  lcd_gotoxy(1,5);
   lcd_putchar(0);
-  lcd_gotoxy(1,1);
-  lcd_putchar(0);
-  lcd_gotoxy(2,1);
-  lcd_putchar(0);
-  lcd_gotoxy(3,0);
-  lcd_putchar(0);
-  lcd_gotoxy(0,15);
-  lcd_putchar(0);
-  lcd_gotoxy(1,14);
-  lcd_putchar(0);
-  lcd_gotoxy(2,14);
-  lcd_putchar(0);
-  lcd_gotoxy(3,15);
-  lcd_putchar(0);
-
-  lcd_gotoxy(1,3);
-  lcd_putstr("Welcome to");
-  lcd_gotoxy(2,4);
-  lcd_putstr("Lab # 4!");
-
+  lcd_putchar(1);
+  lcd_putchar(2);
+  lcd_putchar(3);
+  lcd_gotoxy(2,5);
+  lcd_putchar(4);
+  lcd_putchar(5);
+  lcd_putchar(6);
+  lcd_putchar(7);
+  lcd_gotoxy(3,3);
+  lcd_putstr("...stay cool!");
 }
 
 // Function: read_hex
@@ -289,7 +283,7 @@ void display_char(uint8_t rows[])
     putchar(':');
     putchar(' ');
     byte = rows[i];
-    for(bitmask=1; bitmask <= 16 ; bitmask <<= 1) {
+    for(bitmask=16; bitmask > 0; bitmask >>= 1) {
       if(byte & bitmask) {
         putchar('x');
       } else {
@@ -316,7 +310,7 @@ void display_all_chars()
       putchar(' ');
       lcd_cgram_addr( (charnum<<3) + row);
       byte = lcd_getchar();
-      for(bitmask=1; bitmask <= 16 ; bitmask <<= 1) {
+      for(bitmask=16; bitmask > 0; bitmask >>= 1) {
         if(byte & bitmask) {
           putchar('x');
         } else {
@@ -507,13 +501,13 @@ void display_menu(void)
 {
   printf("\r\n=== Lab 4 Menu ===\r\n\r\n");
   printf(" %c - LCD: Type on screen\r\n", KEY_TYPE);
-  printf(" %c - LCD: Screen contents\r\n", KEY_SCREEN);
+  printf(" %c - LCD: Show screen contents\r\n", KEY_SCREEN);
   printf(" %c - LCD: Custom character\r\n", KEY_CUSTOM);
   printf(" %c - LCD: Dump DDRAM\r\n", KEY_DDRAM);
   printf(" %c - LCD: Dump CGRAM\r\n", KEY_CGRAM);
   printf(" %c - LCD: Clear\r\n", KEY_CLEAR);
   printf(" %c - EEPROM: Write byte\r\n", KEY_EE_WRITE);
-  printf(" %c - EEPROM Read byte\r\n", KEY_EE_READ);
+  printf(" %c - EEPROM: Read byte\r\n", KEY_EE_READ);
   printf(" %c - EEPROM: Dump memory\r\n", KEY_EE_DUMP);
   printf(" %c - COMBO: Save LCD -> EEPROM\r\n", KEY_SAVE);
   printf(" %c - COMBO: Load LCD <- EEPROM\r\n", KEY_LOAD);
